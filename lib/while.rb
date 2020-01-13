4691f707ba1e0c8048caf00237f7cdc4cdc3a353
Tiffany Huang <tiffany.jenyun.huang@gmail.com>
2020-01-13 03:55:46 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
   levtiation_force = 6
-  while levtiation_force < 10
-  puts "Wingardium Leviosa"
-  levtiation_force += 10
-end
+    while levtiation_force < 10
+    puts "Wingardium Leviosa"
+    levtiation_force += 10
+  end
 end
